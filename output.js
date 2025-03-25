b5b4317c3ffe50915431e37bd3711306aa728568
vcelestin <vcelestin@albany.edu>
2025-03-24 20:07:57 -0400

    Add files via upload

---
+++ output.js
@@ -0,0 +1,19 @@
+function showInnerHTML() {
+
+	document.getElementById("output1").innerHTML = "Hi! I've been rotting all day :( ";
+}
+
+function useDocumentWrite() {
+
+	document.write("Hi.. so there's no back button, so you're just going to have to completely restart :( ");
+}
+
+function showAlert() {
+
+	alert(Alert Alert!");
+}
+
+function showConsole() {
+
+	console.log("Hey! You found my secret console message! How are you feeling post break?");
+}
---
+++ variables.js
@@ -0,0 +1,15 @@
+let name = "Vanessa";
+let age = 19;
+let isStudent = true;
+let favoriteColor = "red";
+let favoriteLippie = "Aquaphor";
+
+document.getElementById("demo1".innerHTML = "Name: " + name + "<br>Age: " + age + "<br>Student: " + isStudent;
+
+
+
+// var city = "Albany";
+// let state = "NY";
+// const country = "USA";
+
+//document.getElementById("demo2").innerHTML + "Location: " + city + ", " + country;
